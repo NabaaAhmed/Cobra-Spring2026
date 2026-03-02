276a97860ef85e918c782b07c9366fb03d7ad2a9
Patrick <psherwood85@gmail.com>
2026-03-02 18:18:31 -0500

    Patrick by Patrick

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-
 System.out.println("Hello Guys!");
-system.out.println("Team Cobra Group Project");
-=======
-<<<<<<< HEAD
-//Hi Im mai
-=======
-System.out.println("Hello Guys!");
->>>>>>> a73a225778a660df5028f1f7daac1b27c1ff3e5b
->>>>>>> 03ca485cbd7a33dcf600794073d25739f1d1971a
+System.out.println("Is this the krusty Krab?");
+System.out.println("No, this is Patrick!");
