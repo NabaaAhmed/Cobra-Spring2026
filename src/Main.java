9f7751bbc006ab28a7596c962af8eb7c791e56f6
Mai Duong <locuspocus90@gmail.com>
2026-04-02 20:10:19 -0400

    I added txt files and classes -Mai

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
-System.out.println("Hello Guys!");
-System.out.println("Is this the krusty Krab?");
-System.out.println("No, this is Patrick!");
+public void main(String[] args) {
+    System.out.println("Hello Guys!");
+    System.out.println("Is this the krusty Krab?");
+    System.out.println("No, this is Patrick!");
+}
